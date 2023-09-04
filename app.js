d248c4efc87bf8235330d49b1ed40f83d452087c
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-04 17:54:50 -0400

    Add cors and fix routes.

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const commentsRouter = require('./routes/comment')
-const postsRouter = require('./routes/post')
+const commentsRouter = require('./routes/comment');
+const postsRouter = require('./routes/post');
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
