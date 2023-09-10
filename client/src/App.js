db591ff5c413ec6c0286b6bfaf2a872c1bc6212c
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-09 23:31:13 -0400

    Add Routing and pages of site

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { SubmitPost } from './components/submit-form';
+import { Nav } from './components/nav';
+import { Home } from './components/home'
+import { LogIn } from './components/log-in';
+import { Error } from './components/error'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
+  const [error, setError] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav setError={setError}  />
+        <Routes>
+          <Route path='/' element={<Home setError={setError} />} />
+          <Route path="/post/new"  element={<SubmitPost /> } />
+          <Route path="/:postId/edit"  element={<SubmitPost isEdit={true}/> } />
+          <Route path="/log-in"  element={<LogIn /> } />
+          <Route path='/error' element={<Error error={error}/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
