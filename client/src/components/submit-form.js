43c5cd06a4520dc8db03f0bdcd6d36359619025c
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-12 17:38:02 -0400

    Add some authorization checks

--- client/src/components/submit-form.js
+++ client/src/components/submit-form.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
 export const SubmitPost = (isEdit) => { //receive props from app for when validation fails
@@ -8,12 +9,23 @@ export const SubmitPost = (isEdit) => { //receive props from app for when valida
   const [isPublic, setIsPublic] = useState(false);
   const [errors, setErrors] = useState([])
   const { postId }  = useParams()
+  const navigate = useNavigate();
   // State to control the display of the sign-in modal
+
+  useEffect(() => {
+    const isAuthenticated = () => { // TODO: define in App and pass through as props.
+      const cookie = document.cookie
+      return !!cookie
+    }
+    if (!isAuthenticated()) {
+      navigate('/')
+    }
+  })
 
   const editPost = (e) => {
     e.preventDefault()
     try {
-      fetch('http://localhost:8000/posts', { 
+      fetch(`http://localhost:8000/posts/${postId}`, {  
       method: 'put',
       body : JSON.stringify({
         title: titleInput,
@@ -36,7 +48,7 @@ export const SubmitPost = (isEdit) => { //receive props from app for when valida
   const submitPost = async(e) => {
     e.preventDefault()
     try {
-      const response = fetch('http://localhost:8000/posts', {
+      const response = await fetch('http://localhost:8000/posts', {
         method: 'post',
         body : JSON.stringify({
           title: titleInput,
@@ -45,11 +57,13 @@ export const SubmitPost = (isEdit) => { //receive props from app for when valida
         }),
         headers: {
           'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('connect.sid'),  // TODO: test and add to other requests. https://stackoverflow.com/questions/51478660/how-to-send-jwt-along-with-every-request-after-successful-login
         },
         'Access-Control-Allow-Origin': '*',
         mode: 'cors'
       })
+      console.log(response)
     } catch (error) {
       console.log("error---", error)
     }
