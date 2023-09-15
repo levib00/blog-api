4297ab245147f9939fae592b1df587f99ab1dc44
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-14 23:38:25 -0400

    Add auth checks and error handling

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,37 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import { useNavigate } from "react-router-dom";
 import { Post } from './post';
 
-export const Home = () => {
+export const Home = ({ isAuthenticated, setError }) => {
   const navigate = useNavigate()
-  
-    const fetcher = (url) => fetch(url, { //set localhost port
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      'Access-Control-Allow-Origin': '*',
-      mode: 'cors'
-    })
-    .then(res => res.json())
-    .catch(function(error) {
-      console.log("error---", error)
-    });
-  
-    const {data: posts, error} = useSWR(`http://localhost:8000/posts`, fetcher)
+  const [hasAuth, setHasAuth] = useState()
 
-    useEffect(()=> {
-      if(error) {
-        navigate('/error')
-      }
-    }, [error, navigate])
+  useEffect(()=> {
+    setHasAuth(isAuthenticated())
+  }, [isAuthenticated])
+  
+  const fetcher = (url) => fetch(url, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+    },
+    'Access-Control-Allow-Origin': '*',
+    mode: 'cors'
+  })
+  .then(res => res.json());
+
+  const {data: posts, error} = useSWR(`http://localhost:8000/posts`, fetcher)
+
+  useEffect(()=> {
+    if(error) {
+      setError(error)
+      navigate('/error')
+    }
+  }, [error, navigate])
 
   return (
     <div className="create-post">
       <span>see new posts</span>
-      {posts && posts.map(post => <Post post={post} key={post._id} />)}
+      {posts && posts.map(post => <Post post={post} key={post._id} hasAuth={hasAuth}/>)}
     </div>
   )
 }
