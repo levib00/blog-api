ee108639d9c7f3cecc8bf2668bd989d089243fd6
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-14 23:45:01 -0400

    Change error handling and add some action buttons

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -1,35 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Post = ({post}) => { //receive props from app for when validation fails
+
+export const Post = ({post, hasAuth, setError}) => {
+  const navigate = useNavigate();
   const postDate = new Date(post.timestamp)
 
   const deletePost = () => {
-    fetch(`http://localhost:8000/posts/${post._id}/delete`, { //set localhost port
+    fetch(`http://localhost:8000/posts/${post._id}/delete`, {
       method: 'delete',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
       },
       'Access-Control-Allow-Origin': '*',
       mode: 'cors'
     })
     .then(function(response) {
-      JSON.parse(response)
+      window.location.reload(false)
+      navigate('/')
     })
     // It parses the output
     .catch(function(error) {
-      console.log("error---", error)
+      setError(error)
+      navigate('/error')
     });
   }
 
-  return (
+  return ( // TODO: Add put functions to private and public buttons
     <div className="create-post">
       <div key={post._id}>
         <div>{`${postDate.getDate()}/${postDate.getMonth()}/${postDate.getFullYear()}`}</div>
-        <div>{post.content}</div>
+        <div>{post.title}</div>
         <div className="action-buttons">
-          <button onClick={deletePost}>Delete</button>
-          <a href={`/${post._id}/edit`}><button>Edit</button></a>
+          {hasAuth && <button onClick={deletePost}>Delete</button>}
+          {hasAuth && <a href={`/${post._id}/edit`}><button>Edit</button></a>}
+          {hasAuth && (post.isPublic ? <button>Make private</button> : <button>Make public</button>)} 
         </div>
       </div>
     </div>
