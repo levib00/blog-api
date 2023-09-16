7294089cd83c5e43d0db391924cb00bf3ffb7318
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-16 16:45:32 -0400

    Remove unused import/bad comment

--- user-client/src/components/comment.js
+++ user-client/src/components/comment.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-export const Comment = ({comment}) => { //receive props from app for when validation fails
+export const Comment = ({comment}) => {
   const commentDate = new Date(comment.timestamp)
 
   return (
