0a443c72c2997779b4ad483b7ea804bca2b16e52
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-16 16:48:58 -0400

    Change fetch to SWR and add error handling

--- user-client/src/components/home.js
+++ user-client/src/components/home.js
@@ -1,32 +1,30 @@
 import React, { useState, useEffect } from "react";
 import {Post} from './post';
+import { useNavigate } from 'react-router-dom'
+import useSWR from 'swr';
 
 
-export const Home = () => {
-  
-  const [posts, setPosts] = useState();
-  
+export const Home = ({setError}) => {  
+  const navigate = useNavigate()
 
-  useEffect(() => {
-    const getPosts = async() => {
-      fetch('http://localhost:8000/posts', { //set localhost port
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        'Access-Control-Allow-Origin': '*',
-        mode: 'cors'
-      })
-      .then(async function(response) {
-        setPosts(await response.json())
-      })
-      .catch(function(error) {
-        console.log("error---", error)
-      });
+  const fetcher = (url) => fetch(url, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    'Access-Control-Allow-Origin': '*',
+    mode: 'cors'
+  })
+  .then(res => res.json());
+
+  const {data: posts, error} = useSWR(`http://localhost:8000/posts`, fetcher)
+
+  useEffect(()=> {
+    if(error) {
+      setError(error)
     }
-    getPosts()
-  }, [])
+  }, [error, navigate])
 
   return (
     <div className="create-post">
