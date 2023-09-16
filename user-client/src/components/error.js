a8637d4fa044cadbebf2846cd8cc3af3513ca96e
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-16 16:44:48 -0400

    Add error page

---
+++ user-client/src/components/error.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Error = (error) => {
+  return (
+    <div className="errors">
+      <p>Something went wrong.</p>
+      <h1>{error.status}</h1>
+      <a href="/"> Return to Home</a>
+    </div>
+  )
+}
