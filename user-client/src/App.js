1fba0c8b50ca4e4749b9e4d8552ab385840fbcf6
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-16 18:26:40 -0400

    Add state and props for error handling

--- user-client/src/App.js
+++ user-client/src/App.js
@@ -4,15 +4,18 @@ import {Home} from './components/home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PostPage } from "./components/postPage";
 import { Nav } from './components/nav'
+import { Error } from './components/error'
 
 function App() {
-  return(
+  const [error, setError] = useState(null)
+  return (
     <div>
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/:postId"  element={<PostPage /> } />
+          <Route path='/' element={<Home setError={setError} />} />
+          <Route path="/:postId"  element={<PostPage setError={setError} /> } />
+          <Route path="/error"  element={<Error error={error} /> } />
         </Routes>
       </BrowserRouter>
     </div>
