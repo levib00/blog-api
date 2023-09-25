70144a31155cdb4fbec5533135947dfc1dd7af62
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-24 22:29:19 -0400

    Add classes and icons for styling

--- client/src/components/comment.js
+++ client/src/components/comment.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import Icon from '@mdi/react';
+import { mdiTrashCanOutline } from '@mdi/js';
 
-export const Comment = ({comment, setError, refreshComments}) => {
+export const Comment = ({comment, setError, refreshComments, hasAuth}) => {
   const commentDate = new Date(comment.timestamp)
 
   const navigate = useNavigate();
   
 
   const deleteComment = () => {
-    console.log(comment._id)
     fetch(`http://localhost:8000/comments/${comment._id}/delete`, {
       method: 'delete',
       headers: {
@@ -30,10 +31,14 @@ export const Comment = ({comment, setError, refreshComments}) => {
 
   return (
     <div className="comment">
-      <h3>{comment.displayName}</h3>
-      <div>{`${commentDate.getDate()}/${commentDate.getMonth()}/${commentDate.getFullYear()}`}</div>
-      <div>{comment.content}</div>
-      <button onClick={deleteComment}>delete</button>
+      <div className="comment-text">
+        <div className="comment-info">
+          <h3 className="display-name">{comment.displayName}</h3>
+          <div className="comment-date">{`${commentDate.getDate()}/${commentDate.getMonth()}/${commentDate.getFullYear()}`}</div>
+        </div>
+        <div>{comment.content}</div>
+      </div>
+      {hasAuth && <button className="delete-comment-button" onClick={deleteComment}><Icon path={mdiTrashCanOutline} size={1} /></button>}
     </div>
   )
 }
