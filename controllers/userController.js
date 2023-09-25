0707dff0237f91776da55404f950421b2d2c2464
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-25 00:34:05 -0400

    Remove deleted files.

--- controllers/userController.js
+++
@@ -1,8 +0,0 @@
-import asyncHandler from 'express-async-handler';
-import Comment from '../models/comment';
-
-const UserGet = asyncHandler(async (req, res, next) => {
-  res.send(Comment.findById(req.params.commentId));
-});
-
-export default UserGet;
