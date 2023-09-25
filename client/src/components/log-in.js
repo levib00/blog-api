2bbedfffb107ae6ec1212ed9a09bcfc7c468b6c9
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-24 23:10:20 -0400

    Change auth handling add classes for styling

--- client/src/components/log-in.js
+++ client/src/components/log-in.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-export const LogIn = ({ isAuthenticated, setError }) => { //receive props from app for when validation fails
-  // State to hold the comment input
+export const LogIn = ({ hasAuth, setError, setHasAuth }) => {
   const [usernameInput, setUsernameInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
   const [errors, setErrors] = useState([])
@@ -10,7 +9,7 @@ export const LogIn = ({ isAuthenticated, setError }) => { //receive props from a
   // State to control the display of the sign-in modal
 
   useEffect(() => {
-    if (isAuthenticated()) {
+    if (hasAuth) {
       navigate('/')
     }
   })
@@ -35,6 +34,7 @@ export const LogIn = ({ isAuthenticated, setError }) => { //receive props from a
       if (response.status === 200) {
         const tokenObject = await response.json()
         localStorage.setItem('jwt', await tokenObject.token)
+        setHasAuth(localStorage.getItem('jwt'))
         navigate('/')
       } else if (response.status === 401) {
         setErrors(['Wrong username or password.'])
@@ -48,21 +48,25 @@ export const LogIn = ({ isAuthenticated, setError }) => { //receive props from a
   }
 
   return (
-    <div className="create-post">
-      <form>
-        <label htmlFor='post-title'>username</label>
-        <input id='post-title' type='text' onChange={(e) => setUsernameInput(e.target.value)} value={usernameInput}></input>
-        {/* Textarea for entering the comment */}
-        <label  htmlFor='post-textbox'>password</label>
-        <input type="text" id='post-textbox' className="comment-textbox" onChange={(e) => setPasswordInput(e.target.value)} value={passwordInput}></input>
-        {/* Button to save the comment */}
-        <button type="submit" className="submit-post-button" onClick={ (e) => login(e) }>save</button>
-      </form>
-      {errors.length > 0 ? <ul>
-        {errors.map(error => <li>{error}</li>)}
-      </ul>
-      :
-      null}
+    <div className="content">
+      <div className="sign-in-container">
+        <form className="sign-in-form">
+          <div>
+            <div className="label-container"><label htmlFor='username' className="username-label">Username:</label></div>
+            <input id='username' className="username sign-in-input" type='text' onChange={(e) => setUsernameInput(e.target.value)} value={usernameInput}></input>
+          </div>
+          <div>
+            <div className="label-container"><label  htmlFor='password' className="password-label">Password:</label></div>
+            <input type="password" id='password' className="password sign-in-input" onChange={(e) => setPasswordInput(e.target.value)} value={passwordInput}></input>
+          </div>
+          <button type="submit" className="sign-in-button" onClick={ (e) => login(e) }>Sign in</button>
+        </form>
+        {errors.length > 0 ? <ul>
+          {errors.map(error => <li>{error}</li>)}
+        </ul>
+        :
+        null}
+      </div>
     </div>
   )
 }
