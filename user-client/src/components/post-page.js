49c359e654b52c33c236e2a0b3178dc669e7b98f
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-25 00:31:43 -0400

    Add post-page for editing and deleting comments.

---
+++ user-client/src/components/post-page.js
@@ -0,0 +1,60 @@
+import React, {  useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { Post } from './post';
+import { Comment } from './comment';
+import { SubmitComment } from './submit-comment';
+import useSWR from 'swr';
+
+export const PostPage = ({setError, parseDom}) => {
+  const navigate = useNavigate()
+  const { postId }  = useParams()
+
+  const fetcher = async url => {
+      const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      'Access-Control-Allow-Origin': '*',
+      mode: 'cors'
+    })
+    console.log(res)
+
+    if (!res.ok) {
+      const error = new Error('An error occurred while fetching the data.')
+      error.info = await res.text()
+      error.status = res.status
+      console.log({error})
+      throw error
+    }
+
+    return res.json()
+  }
+
+  const {data: post, error: postError} = useSWR(`http://localhost:8000/posts/${postId}`, fetcher)
+  const {data: comments, mutate, error: commentError} = useSWR(`http://localhost:8000/comments/${postId}`, fetcher)
+
+  console.log({post, comments, postError, commentError})
+
+  useEffect(()=> {
+    if(postError) {
+      setError(postError)
+      navigate('/error')
+    }
+  }, [commentError, postError, navigate])
+
+  return (
+    <div className="content">
+        <div className="post-page-post">
+          {post && <Post postId={postId} post={post[0]} parseDom={parseDom}/>}
+        </div>
+      <div className="comment-section">
+        <SubmitComment postId={postId} refreshComments={mutate} setError={setError}/>
+        <div className="comments-container">
+          {comments && comments.map(comment => <Comment key={comment._id} parseDom={parseDom} comment={comment} />)}
+        </div>
+      </div>
+    </div>
+  )
+}
