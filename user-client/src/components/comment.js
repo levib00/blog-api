90cd0cf996568bd996ab6c8ae56b25e5dae33daa
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-25 00:27:39 -0400

    Change dom for styling

--- user-client/src/components/comment.js
+++ user-client/src/components/comment.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-export const Comment = ({comment}) => {
+export const Comment = ({comment, parseDom}) => {
   const commentDate = new Date(comment.timestamp)
 
   return (
     <div className="comment">
-      <h3>{comment.displayName}</h3>
-      <div>{`${commentDate.getDate()}/${commentDate.getMonth()}/${commentDate.getFullYear()}`}</div>
-      <div>{comment.content}</div>
+      <div className="comment-info">
+        <h3>{parseDom(comment.displayName)}</h3>
+        <span>{`${commentDate.getDate()}/${commentDate.getMonth()}/${commentDate.getFullYear()}`}</span>
+      </div>
+      <p className="comment-content">{parseDom(comment.content)}</p>
     </div>
   )
 }
