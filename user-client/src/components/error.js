933be56671a204fe0339fc2e2019dbb8e07c7a2e
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-25 00:28:11 -0400

    Add info message.

--- user-client/src/components/error.js
+++ user-client/src/components/error.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-export const Error = (error) => {
+export const Error = ({error}) => {
   return (
     <div className="errors">
       <p>Something went wrong.</p>
       <h1>{error.status}</h1>
+      <p>{error.info}</p>
       <a href="/"> Return to Home</a>
     </div>
   )
