60b25affad2164a31cb89453a624b1662e7003a2
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-24 22:29:46 -0400

    Add info message and class for styling

--- client/src/components/error.js
+++ client/src/components/error.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export const Error = (error) => {
+export const Error = ({error}) => {
 
   return (
-    <div className="errors">
+    <div className="content">
       <p>Something went wrong.</p>
       <h1>{error.status}</h1>
+      <p>{error.info}</p>
       <a href="/"> Return to Home</a>
     </div>
   )
