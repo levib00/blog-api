95f57bc28f349ebf066f60c272c158d97d5ee011
levib00 <bekurkes@DESKTOP-B2V4ILA>
2023-09-25 00:27:04 -0400

    Add footer, styles, and parser for special characters

--- user-client/src/App.js
+++ user-client/src/App.js
@@ -2,21 +2,30 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 import {Home} from './components/home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PostPage } from "./components/postPage";
+import { PostPage } from "./components/post-page";
 import { Nav } from './components/nav'
 import { Error } from './components/error'
+import { Footer } from './components/footer'
+import './styles/styles.css'
 
 function App() {
   const [error, setError] = useState(null)
+
+  const parseDom = (str) => {
+    const doc = new DOMParser().parseFromString(str, "text/html");
+    return doc.documentElement.textContent;
+  }
+
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path='/' element={<Home setError={setError} />} />
-          <Route path="/:postId"  element={<PostPage setError={setError} /> } />
+          <Route path='/' element={<Home setError={setError} parseDom={parseDom} />} />
+          <Route path="/:postId"  element={<PostPage setError={setError} parseDom={parseDom} /> } />
           <Route path="/error"  element={<Error error={error} /> } />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
