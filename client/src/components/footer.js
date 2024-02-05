a38d09968cd5966e8dca3d00f30d39b516b32c38
Levi B <levib2000@gmail.com>
2024-02-05 03:35:37 -0500

    Remove clients

--- client/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return (
-    <footer className="footer">
-      <p>created by <a className="github-link" href="https://github.com/levib00/blog-api">levib00</a> on github</p>
-    </footer>
-  )
-}
--- user-client/src/components/nav.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Nav = () => {
-  return (
-    <div className="nav">
-      <h1><a href="/">Home</a></h1>
-    </div>
-  )
-}
