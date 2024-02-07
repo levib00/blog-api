79b3e21a8b27e99c4cb05465ca83096cfb3d0608
Levi B <levib2000@gmail.com>
2024-02-07 03:52:53 -0500

    Add comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,23 +1,27 @@
-const { body, validationResult } = require('express-validator');
-const asyncHandler = require('express-async-handler');
-const Comment = require('../models/comment');
+const { body, validationResult } = require('express-validator'); // Importing express-validator for validating request bodies
+const asyncHandler = require('express-async-handler'); // Importing express-async-handler for handling asynchronous functions in routes
+const Comment = require('../models/comment'); // Importing the Comment model
 
-exports.AllCommentsGet = asyncHandler(async (req, res, next) => {
-  res.send(await Comment.find());
+// Route handler for getting all comments
+exports.AllCommentsGet = asyncHandler(async (req, res) => {
+  res.send(await Comment.find()); // Sending all comments found in the database
 });
 
-exports.PostPageCommentsGet = asyncHandler(async (req, res, next) => {
+// Route handler for getting comments of a specific post
+exports.PostPageCommentsGet = asyncHandler(async (req, res) => {
+  // Finding comments associated with a specific post ID
   const comments = await Comment.find({ parentPost: req.params.postId }).exec();
-  res.send(comments);
+  res.send(comments); // Sending the found comments
 });
 
-exports.CommentGet = asyncHandler(async (req, res, next) => {
-  res.send(await Comment.findById(req.params.commentId));
+// Route handler for getting a single comment by ID
+exports.CommentGet = asyncHandler(async (req, res) => {
+  res.send(await Comment.findById(req.params.commentId)); // Finding and sending a comment by its ID
 });
 
+// Route handler for posting a new comment
 exports.CommentPost = [
-
-  // Validate and sanitize fields.
+  // Validation and sanitization of request body fields
   body('content', 'Comment must not be empty.')
     .trim()
     .isLength({ min: 3 })
@@ -35,12 +39,12 @@ exports.CommentPost = [
   body('parentPost')
     .escape(),
 
-  // Process request after validation and sanitization.
-
-  asyncHandler(async (req, res, next) => {
-    // Extract the validation errors from a request.
+  // Route handler for processing the request after validation and sanitization
+  asyncHandler(async (req, res) => {
+    // Extract the validation errors from the request
     const errors = validationResult(req);
 
+    // Creating a new comment object with data from the request body
     const comment = new Comment({
       parentPost: req.body.parentPost,
       content: req.body.content,
@@ -48,6 +52,7 @@ exports.CommentPost = [
       timestamp: Date.now(),
     });
 
+    // Checking for validation errors
     if (!errors.isEmpty()) {
       res.status(422).send({
         comment,
@@ -55,19 +60,23 @@ exports.CommentPost = [
       });
     } else {
       try {
-        await comment.save();
-        res.send('comment has been saved');
+        await comment.save(); // Saving the new comment to the database
+        res.send('comment has been saved'); // Sending a success message
       } catch (error) {
-        res.status(500).send({ msg: 'something went wrong' });
+        res.status(500).send({ msg: 'something went wrong' }); // Sending an error message if something goes wrong
       }
     }
   }),
 ];
 
-exports.CommentDelete = asyncHandler(async (req, res, next) => {
-  if (Comment.findById(req.params.commentId)) {
-    await Comment.findByIdAndRemove(req.params.commentId);
-    return res.send('Comment has been deleted.');
+// Route handler for deleting a comment by ID
+exports.CommentDelete = asyncHandler(async (req, res) => {
+  const comment = await Comment.findById(req.params.commentId); // Finding the comment by its ID
+
+  // Checking if the comment exists
+  if (comment) {
+    await Comment.findByIdAndRemove(req.params.commentId); // Removing the comment from the database
+    return res.send('Comment has been deleted.'); // Sending a success message
   }
-  return res.status(404).send('Comment could not be found.');
+  return res.status(404).send('Comment could not be found.'); // Sending a not found message if the comment does not exist
 });
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,40 +1,40 @@
-const { body, validationResult } = require('express-validator');
-const asyncHandler = require('express-async-handler');
-const jwt = require('jsonwebtoken');
-const Post = require('../models/post');
-const Comment = require('../models/comment');
-require('dotenv').config();
+const { body, validationResult } = require('express-validator'); // Importing express-validator for validating request bodies
+const asyncHandler = require('express-async-handler'); // Importing express-async-handler for handling asynchronous functions in routes
+const Post = require('../models/post'); // Importing the Post model
+const Comment = require('../models/comment'); // Importing the Comment model
+require('dotenv').config(); // Importing dotenv for environment variables
 
+// Route handler for getting all posts
 exports.AllPostsGet = asyncHandler(async (req, res) => {
   let posts;
-  jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
-    if (err) {
-      posts = await Post.find({ isPublic: true });
-    } else {
-      posts = await Post.find();
-    }
-    res.send(posts);
-  });
+  // If user is not logged in, only show public posts
+  if (req.user === null) {
+    posts = await Post.find({ isPublic: true });
+  } else {
+    posts = await Post.find(); // Otherwise, show all posts
+  }
+  res.send(posts);
 });
 
+// Route handler for getting a single post by ID
 exports.PostGet = asyncHandler(async (req, res) => {
   let post = await Post.find({ isPublic: true, _id: req.params.postId });
-  jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
-    if (post.length > 0) {
-      res.send(post);
-    } else if (err) {
+  // Show the post if it's public or the user has access to it
+  if (post.length > 0) {
+    res.send(post);
+  } else if (req.user === null) {
+    res.status(404).send('This post does not exist or you do not have access to it');
+  } else {
+    post = await Post.findById(req.params.postId);
+    if (post.length < 1) {
       res.status(404).send('This post does not exist or you do not have access to it');
     } else {
-      post = await Post.findById(req.params.postId);
-      if (post.length < 1) {
-        res.status(404).send('This post does not exist or you do not have access to it');
-      } else {
-        res.send(post);
-      }
+      res.send(post);
     }
-  });
+  }
 });
 
+// Route handler for posting a new post
 exports.PostPost = [
   // Validate and sanitize fields.
   body('title', 'Post content must not be empty.')
@@ -48,43 +48,39 @@ exports.PostPost = [
   body('isPublic'),
 
   // Process request after validation and sanitization.
-
   asyncHandler(async (req, res) => {
-    // Extract the validation errors from a request.
-    jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
-      if (err) {
-        res.status(401).send('You must be logged in as an admin to post.');
-      } else {
-        const errors = validationResult(req);
+    if (req.user === null) {
+      res.status(401).send('You must be logged in as an admin to post.');
+    } else {
+      const errors = validationResult(req);
 
-        const post = new Post({
-          content: req.body.content,
-          title: req.body.title,
-          timestamp: Date.now(),
-          isPublic: req.body.isPublic,
+      const post = new Post({
+        content: req.body.content,
+        title: req.body.title,
+        timestamp: Date.now(),
+        isPublic: req.body.isPublic,
+      });
+
+      if (!errors.isEmpty()) {
+        res.status(422).send({
+          errors: errors.array(),
+          post,
         });
-
-        if (!errors.isEmpty()) {
-          res.status(422).send({
-            errors: errors.array(),
-            post,
-          });
-        } else {
-          // Data from form is valid. Save post.
-          try {
-            await post.save();
-            res.send('post has been saved');
-          } catch (error) {
-            res.send(error);
-          }
+      } else {
+        // Data from form is valid. Save post.
+        try {
+          await post.save();
+          res.send('post has been saved');
+        } catch (error) {
+          res.send(error);
         }
       }
-    });
+    }
   }),
 ];
 
+// Route handler for editing a post
 exports.editPostPut = [
-
   // Validate and sanitize fields.
   body('title', 'Post content must not be empty.')
     .trim()
@@ -97,56 +93,49 @@ exports.editPostPut = [
   body('isPublic'),
 
   // Process request after validation and sanitization.
-
   asyncHandler(async (req, res) => {
-    jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
-      if (err) {
-        res.status(401).send('You must be logged in as an admin to edit a post.');
-      } else {
-        const errors = validationResult(req);
+    if (req.user === null) {
+      res.status(401).send('You must be logged in as an admin to edit a post.');
+    } else {
+      const errors = validationResult(req);
 
-        const post = new Post({
-          content: req.body.content,
-          title: req.body.title,
-          isPublic: req.body.isPublic,
-          timestamp: req.body.timestamp,
-          _id: req.params.postId,
+      const post = new Post({
+        content: req.body.content,
+        title: req.body.title,
+        isPublic: req.body.isPublic,
+        timestamp: req.body.timestamp,
+        _id: req.params.postId,
+      });
+
+      if (!errors.isEmpty()) {
+        res.status(422).send({
+          errors: errors.array(),
+          post,
         });
-
-        if (!errors.isEmpty()) {
-          res.status(422).send({
-            errors: errors.array(),
-            post,
-          });
-        } else {
-          // Data from form is valid. Save post.
-          try {
-            try {
-              await Post.findByIdAndUpdate(req.params.postId, post, {});
-              res.send({ post, msg: 'post has been updated.' });
-            } catch (error) {
-              res.send(error);
-            }
-          } catch (error) {
-            res.send(error);
-          }
+      } else {
+        // Data from form is valid. Update post.
+        try {
+          await Post.findByIdAndUpdate(req.params.postId, post, {});
+          res.send({ post, msg: 'post has been updated.' });
+        } catch (error) {
+          res.send(error);
         }
       }
-    });
+    }
   }),
 ];
 
+// Route handler for deleting a post
 exports.PostDelete = asyncHandler(async (req, res) => {
-  jwt.verify(req.token, process.env.JWT_SECRET, async (err) => {
-    const post = await Post.findById(req.params.postId);
-    if (err) {
-      res.status(401).send('You must be logged in as an admin to delete a post.');
-    } else if (post) {
-      await Comment.deleteMany({ parentPost: req.params.postId });
-      await Post.findByIdAndRemove(req.params.postId);
-      res.send('Comment has been deleted.');
-    } else {
-      res.status(404).send('This post was not found');
-    }
-  });
+  // Delete the selected post and all child comments.
+  const post = await Post.findById(req.params.postId);
+  if (req.user === null) {
+    res.status(401).send('You must be logged in as an admin to delete a post.');
+  } else if (post) {
+    await Comment.deleteMany({ parentPost: req.params.postId });
+    await Post.findByIdAndRemove(req.params.postId);
+    res.send('Post and its comments have been deleted.');
+  } else {
+    res.status(404).send('This post was not found');
+  }
 });
