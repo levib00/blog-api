6309fdc7d41fdf6ad3015e7d2867853c8c3ca69b
Levi B <levib2000@gmail.com>
2024-02-07 03:57:24 -0500

    Add semicolon

--- routes/comment.js
+++ routes/comment.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const commentController = require('../controllers/commentController');
-const verifyToken = require('../middleware/verifyToken')
+const verifyToken = require('../middleware/verifyToken');
 
 const router = express.Router();
 
